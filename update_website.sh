e22720f31b344337d659813689c1d45eb5a2ebfc
Raul HC <raul.herrador.colino@gmail.com>
2016-11-18 12:31:23 +0100

    Added post about internship interview at CSC (viernes, 18 de noviembre de 2016, 12:31:22 CET)

--- update_website.sh
+++ update_website.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-buster generate
-git add -A
-git commit -m "Update on the website at $(date)"
+# This script  is used to update my ghost blog (hosted on github) everytime I add something new
+
+# Generates static pages from locally running Ghost instance.
+buster generate						
+
+# Git workflow
+git add -A 							
+echo "Enter commit message: "
+read input_var
+date_var = date
+git commit -m "$input_var (`date`)" # Current date is added 
 git push origin master
